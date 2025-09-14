3503d36a7141fce9040a338a803bcf6dd6efc4fc
shamilkc <shamil@yougotagift.com>
2025-09-14 15:47:11 +0530

    custom pagination

--- shiperp/settings.py
+++ shiperp/settings.py
@@ -154,10 +154,9 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
     ],
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'DEFAULT_PAGINATION_CLASS': 'base.api.pagination.CustomPagination',
     'PAGE_SIZE': 10,
 }
-
 AUTHENTICATION_BACKENDS = [
     'django.contrib.auth.backends.ModelBackend',
 ]
