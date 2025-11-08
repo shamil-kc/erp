4c89b30cfb156c5f03a8b71050870042558a39df
shamilkc <shamil@yougotagift.com>
2025-11-08 09:47:26 +0530

    create payment entry removed

--- base/api/serializers.py
+++ base/api/serializers.py
@@ -247,14 +247,6 @@ class PurchaseInvoiceCreateSerializer(serializers.ModelSerializer):
             except Exception as e:
                 transaction.set_rollback(True)
                 raise e
-
-            # create payment entries
-            for payment in payments_data:
-                PaymentEntry.objects.create(invoice_id=invoice.id,
-                                            invoice_type='purchase',
-                    payment_type=payment['payment_type'],
-                    amount=payment['amount'],
-                    created_by=self.context['request'].user)
         return invoice
 
 class PurchaseInvoiceUpdateSerializer(serializers.ModelSerializer):
@@ -488,13 +480,6 @@ class SaleInvoiceCreateSerializer(serializers.ModelSerializer):
             Commission.objects.create(sales_invoice=invoice, **commission_data)
 
         invoice.calculate_totals()
-
-        # create payment entries
-        for payment in payments_data:
-            PaymentEntry.objects.create(invoice_id=invoice.id,
-                                        invoice_type='sale',
-                payment_type=payment['payment_type'], amount=payment['amount'],
-                created_by=self.context['request'].user)
         return invoice
 
 class SaleInvoiceUpdateSerializer(serializers.ModelSerializer):
