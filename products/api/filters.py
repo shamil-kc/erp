f3536e2255fe087f806bf09f4bfff04adcab31c9
shamilkc <shamil@yougotagift.com>
2025-11-18 21:04:19 +0530

    stock filter added

--- products/api/filters.py
+++ products/api/filters.py
@@ -4,6 +4,7 @@ from products.models import ProductItem
 
 class ProductItemFilter(django_filters.FilterSet):
     search = django_filters.CharFilter(method='filter_search')
+    is_stock = django_filters.BooleanFilter(method='filter_is_stock')
 
     def filter_search(self, queryset, name, value):
         return queryset.filter(
@@ -13,6 +14,12 @@ class ProductItemFilter(django_filters.FilterSet):
             models.Q(grade__product_type__product__name__icontains=value)
         )
 
+    def filter_is_stock(self, queryset, name, value):
+        if value:
+            return queryset.filter(stock__quantity__gt=0)
+        else:
+            return queryset.filter(stock__quantity__lte=0)
+
     class Meta:
         model = ProductItem
-        fields = ['search']
+        fields = ['search', 'is_stock']
