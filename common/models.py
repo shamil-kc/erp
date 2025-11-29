a945f328d85f340b4c28f1f4f035e7b9048fe093
shamilkc <shamil@yougotagift.com>
2025-11-29 10:09:36 +0530

    extra charges model

--- common/models.py
+++ common/models.py
@@ -149,3 +149,26 @@ class Asset(models.Model):
 
     def __str__(self):
         return f"{self.name} ({self.status})"
+
+
+class ExtraCharges(models.Model):
+
+    invoice_type = models.CharField(max_length=10,
+        choices=(('sale', 'Sale'), ('purchase', 'Purchase')),
+        help_text='Type of invoice this payment belongs to')
+
+    invoice_id = models.PositiveIntegerField(
+        help_text='ID of related Sale or Purchase invoice')
+
+    amount = models.DecimalField(max_digits=14, decimal_places=2)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+    created_by = models.ForeignKey(User, on_delete=models.SET_NULL,
+                                   null=True)
+
+    class Meta:
+        verbose_name_plural = "Payment Entries"
+
+    def __str__(self):
+        return f"{self.invoice_type} invoice #{self.invoice_id}"
