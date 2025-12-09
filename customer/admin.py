192ca45b6db1f8bbcd944e5c547043796048af72
shamilkc <shamil@yougotagift.com>
2025-12-09 21:18:26 +0530

    party admin added

--- customer/admin.py
+++ customer/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Party
+
+@admin.register(Party)
+class PartyAdmin(admin.ModelAdmin):
+    list_display = ('name', 'type', 'phone', 'email', 'company_name', 'trn', 'created_at', 'modified_at')
+    search_fields = ('name', 'company_name', 'trn', 'email', 'phone')
+    list_filter = ('type', 'created_at', 'modified_at')
+    ordering = ('-created_at',)
