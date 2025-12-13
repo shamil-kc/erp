f1ae75d185df662a4fb0734579613d152ca9a71b
shamilkc <shamil@yougotagift.com>
2025-12-13 11:13:07 +0530

    trn search added

--- customer/api/filters.py
+++ customer/api/filters.py
@@ -4,8 +4,9 @@ from customer.models import Party
 class PartyFilter(django_filters.FilterSet):
     name = django_filters.CharFilter(field_name='name', lookup_expr='icontains')
     company_name = django_filters.CharFilter(field_name='company_name', lookup_expr='icontains')
+    trn = django_filters.CharFilter(field_name='trn', lookup_expr='icontains')
 
     class Meta:
         model = Party
-        fields = ['type', 'name', 'company_name']
+        fields = ['type', 'name', 'company_name', 'trn']
 
