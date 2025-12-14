ae37b62d72ae9d6da970b699ac9e45674e7169d8
shamilkc <shamil@yougotagift.com>
2025-12-14 19:39:41 +0530

    capital admin add

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import CapitalAccount
 
-# Register your models here.
+
+@admin.register(CapitalAccount)
+class CapitalAccountAdmin(admin.ModelAdmin):
+    list_display = ('name', 'balance', 'created_at', 'modified_at')
+    search_fields = ('name',)
+    readonly_fields = ('created_at', 'modified_at')
