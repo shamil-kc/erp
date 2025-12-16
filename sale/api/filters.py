576baa191469a979a4789d0774db479908a6d026
shamilkc <shamil@yougotagift.com>
2025-12-16 21:53:26 +0530

    search added

--- sale/api/filters.py
+++ sale/api/filters.py
@@ -7,10 +7,11 @@ class SaleInvoiceFilter(django_filters.FilterSet):
     sale_date__lte = django_filters.DateFilter(field_name='sale_date', lookup_expr='lte')
     status = django_filters.CharFilter(field_name='status', lookup_expr='iexact')
     party_id = django_filters.NumberFilter(field_name='party_id')
+    invoice_no = django_filters.CharFilter(field_name='invoice_no', lookup_expr='icontains')
 
     class Meta:
         model = SaleInvoice
-        fields = ['sale_date__gte', 'sale_date__lte', 'status', 'party_id']
+        fields = ['sale_date__gte', 'sale_date__lte', 'status', 'party_id', 'invoice_no']
 
 
 class SaleReturnItemFilter(django_filters.FilterSet):
