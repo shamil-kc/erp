8b27a72ca9e3df4d27d918523ad2eb6250f0414f
shamilkc <shamil@yougotagift.com>
2025-12-20 17:34:13 +0530

    sale invoice filter using purchase item

--- sale/api/filters.py
+++ sale/api/filters.py
@@ -8,10 +8,30 @@ class SaleInvoiceFilter(django_filters.FilterSet):
     status = django_filters.CharFilter(field_name='status', lookup_expr='iexact')
     party_id = django_filters.NumberFilter(field_name='party_id')
     invoice_no = django_filters.CharFilter(field_name='invoice_no', lookup_expr='icontains')
+    has_purchase_item = django_filters.BooleanFilter(method='filter_has_purchase_item')
+    purchase_item_ids = django_filters.BaseInFilter(
+        field_name='sale_items__purchase_item_id', method='filter_by_purchase_item_ids'
+    )
 
     class Meta:
         model = SaleInvoice
-        fields = ['sale_date__gte', 'sale_date__lte', 'status', 'party_id', 'invoice_no']
+        fields = [
+            'sale_date__gte', 'sale_date__lte', 'status', 'party_id',
+            'invoice_no', 'has_purchase_item', 'purchase_item_ids'
+        ]
+
+    def filter_has_purchase_item(self, queryset, name, value):
+        if value:
+            # Only invoices where at least one sale item has a purchase_item set
+            return queryset.filter(sale_items__purchase_item__isnull=False).distinct()
+        else:
+            # Only invoices where all sale items have no purchase_item
+            return queryset.filter(sale_items__purchase_item__isnull=True).distinct()
+
+    def filter_by_purchase_item_ids(self, queryset, name, value):
+        if value:
+            return queryset.filter(sale_items__purchase_item_id__in=value).distinct()
+        return queryset
 
 
 class SaleReturnItemFilter(django_filters.FilterSet):
